72490f02844e3c4c96544ebe5adafa4ea3c04f06
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-10-04 16:03:49 +0530

    Add files via upload

---
+++ jikku111.py
@@ -0,0 +1,81 @@
+#print("Hello, World!")
+#print("Hello\nWorld")
+'''
+if 5 > 3:
+  print("Five is greater than two!")
+
+
+
+if 5 > 2:
+  print("Five is greater than two!")
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+            print("Five is greater than two!")
+
+
+if 5 > 2:
+ print("Five is greater than two!")
+ print("Five is greater than two!")
+
+
+x = 5
+x = "thomas"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+x = 1j
+y = "John"
+print(type(x))
+print(type(y))
+
+'''
+
+
+'''
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y +
+
+
+
+
+'''
+'''
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+'''
+
+
+a = "Hello, World!"
+print(a[6])
+
+
+b = "Hello, World!"
+print(b[:5])
+
+
